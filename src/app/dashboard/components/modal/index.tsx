d1b189c29ee9cb87caf2365723ae54c3ab7c4ff0
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-11-15 21:06:22 -0300

    Add order total calculation and update modal display with total value

--- src/app/dashboard/components/modal/index.tsx
+++ src/app/dashboard/components/modal/index.tsx
@@ -3,6 +3,7 @@ import { X } from 'lucide-react';
 import styles from './styles.module.scss';
 import {use} from 'react'
 import { OrderContext } from '@/providers/order';
+import { calculateTotalOrder } from '@/lib/helper';
 
 export function Modalorder() {
     const { onRequestClose, order, finishOrder} = use(OrderContext)
@@ -28,13 +29,17 @@ export function Modalorder() {
                 )}
              {order.map(item => (
                    <section key={item.id} className={styles.item}>
-                    <span>{item.amount} - <b>{item.product.name}</b></span>
+                    <span>Qtd: {item.amount} - <b>{item.product.name}</b> - R$ {parseFloat(item.product.price) *
+                    item.amount
+                    }</span>
                     <span className={styles.description}> 
                         {item.product.description}
                         </span>
                 </section>
 
              ) )}
+
+             <h3 className={styles.total}>Valor total: R$ {calculateTotalOrder(order)}</h3>
                 <button className={styles.buttonOrder} onClick={handleFinishOrder}>
                     Concluir pedido
                 </button>
